7641f00fcb9da99d96feed2d137a8972d713ffb7
SUSHMITHA <76254481+sushmitha286@users.noreply.github.com>
2023-03-08 11:57:48 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Factorial{
+static int fact(int n){
+    int res = 1;
+    for( int i =1;i<=n;i++){
+        res = res*i;
+    }
+    return res;
+}
+
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(fact(n));
+    }
+}
+
+
